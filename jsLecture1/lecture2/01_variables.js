3fb914eda0635d0f9f99cf9f86c92226fe3fa7bc
SameenFatima <sameen.fatima993@gmail.com>
2025-05-30 11:48:35 +0500

    lecture_1

---
+++ jsLecture1/lecture2/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 123456
+let accountEmail = "sameen@gmail.com"
+var accountPasswerd = "876543"
+let accountStte;
+// let accountState; is men ; ka matlab undefiend abhi value nhi pta pr jaga is ny ly li//
+accountCity = "karachi"
+
+// accountId ="459034"
+// const sy verables bnaty hen to us ki value change nhi ho skti is liye const use krty hen
+ accountEmail = " hooriya@hotmail.com"
+ accountPasswerd ="5757578"
+ accountCity = "hydrabad"
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountPasswerd , accountCity , accountStte])
+// ek sath console krny ka tariqa //
+
+
+/*
+prefere not to use "var" 
+because of block scope anf functional scope
+*/
