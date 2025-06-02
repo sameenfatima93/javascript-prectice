ba52822b220ea4a1d6c36dae0dbe248515d47723
SameenFatima <sameen.fatima993@gmail.com>
2025-06-02 11:47:43 +0500

    operations

---
+++ jsLecture3-4/03_conversionAndOperating.js
@@ -0,0 +1,23 @@
+// let score = "33"
+// console.log(typeof(score));
+// dono tariqy drst hen //
+//if you need to string convert into number //
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+
+// let isLogedIn = 1 // if convert 1 in to boolean resyult should be i "true" 
+
+//  let isLogedIn = "" // if convert empty into boolean result should be  "false"
+let isLogedIn = "sameen" //if string convert into boolean so result should be "true"
+
+let booleanIsLogedIn = Boolean(isLogedIn)
+// console.log (booleanIsLogedIn);
+
+//convert number to string
+ let myNumber = 4954
+ let myStringNumber = String(myNumber)
+ console.log( typeof myStringNumber);
+ 
---
+++ jsLecture3-4/04_operations.js
@@ -0,0 +1,23 @@
+// *******operaton*********//
+
+let value = 4
+let negValue = -value // -(minus ky sign sy) sy koi bhi value minus me convert ho jati hy  jesy yahan value sy pehly minus lga diya
+//  console.log(negValue);
+// console.log(3%5);
+
+// concrinate //
+ let str1 = "sameen"
+ let str2 = " fatima"
+ let str3 = str1 + str2;
+//  console.log(str3);
+ 
+// console.log(true); // khali true true hy 
+
+console.log( +true); // true ky sath + lgaden to wph answer 1 dyga true apny ap me boolean hy ap ny us me conversion krna hy incriment krna hy
+
+console.log( +""); // is ky answer men 0 aaye ga
+
+ 
+
+
+ 
