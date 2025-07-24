112ad47e6d487407e3f86b84bd04b2df7622d39b
SameenFatima <sameen.fatima993@gmail.com>
2025-07-24 08:13:04 +0500

    string_prototype

---
+++ lecture_10(stringInJavaScript)/06_stringInJs.js
@@ -0,0 +1,29 @@
+// most commpon and simple way to concetinate//
+// var name = "sameen"
+// var age = 20
+// console.log(name + age + " value");
+
+// onother and smart way to concetinate with using bactics ` this is called "string intopoletion"//
+//  var name = "sameen"
+//  var age = 20
+//  var studyIn = "java Script"
+//  console.log(`hello my name is ${name} and my age is ${age} now i am learning ${studyIn}`);
+
+ //secondway to declare any string//
+
+ const gameName = new String('hitashhc')
+ console.log(gameName)
+ console.log(gameName.small())
+ console.log(gameName.length)
+ console.log(gameName.toUpperCase())
+ console.log(gameName.slice())
+ console.log(gameName.split())
+ console.log(gameName.valueOf())
+ //these are and so many prototype is decliared in new string () 
+
+ 
+
+
+
+ 
+
