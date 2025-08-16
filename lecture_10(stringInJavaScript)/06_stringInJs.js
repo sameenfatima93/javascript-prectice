7eca4d9e831ed74191707333ebfce491cfb0495f
SameenFatima <sameen.fatima993@gmail.com>
2025-08-16 07:56:23 +0500

    lecture10Done

--- lecture_10(stringInJavaScript)/06_stringInJs.js
+++ lecture_10(stringInJavaScript)/06_stringInJs.js
@@ -11,15 +11,78 @@
 
  //secondway to declare any string//
 
- const gameName = new String('hitashhc')
- console.log(gameName)
- console.log(gameName.small())
- console.log(gameName.length)
- console.log(gameName.toUpperCase())
- console.log(gameName.slice())
- console.log(gameName.split())
- console.log(gameName.valueOf())
+//  const gameName = 'hitashhc';
+//  console.log(gameName)
+//  console.log(gameName.small())
+//  console.log(gameName.length)
+//  console.log(gameName.toUpperCase()) 
+//  console.log(gameName.slice())
+//  console.log(gameName.split())
+//  console.log(gameName.valueOf())
  //these are and so many prototype is decliared in new string () 
+
+// console.log(__proto__);
+// console.log(__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf(`s`));
+
+// substring and slice
+
+// const newGameName = gameName.substring(2,4);
+// console.log(newGameName); 
+// substring bhi slice ki tarha copy kr dyta pr is me negaive velue nhu dy skty
+
+// let gameName = 'hitashhc';
+
+// const newGameName = gameName.slice(-8, 4);
+// let onotherName = gameName.slice(-8, 2)
+// console.log(onotherName);
+
+// .trim ye white spase ko remove krta hy jis ki zarort nhi hoti
+// const newstringOne = "      sameeen    "
+// console.log(newstringOne);
+// console.log(newstringOne.trim());
+// 
+
+
+// replace input sy mli hoi ksi string men chaning krni ho to
+// for wexample
+// const url = "https://sameenfatima%20dev/sameenfatiima%20dev.com"
+
+// console.log(url.replaceAll(`%20` , `_`)); 
+// yaha pehly jo value hy woh dalni hy phir jis sy replace krna hy woh dalna hy
+// values found krny ky liye .incolude lgaty
+// console.log(url.includes(`fatima`));
+// console.log(url.includes(`furqan`));
+
+// split mathode
+const str = "The quick brown fox jumps over the lazy dog.";
+
+const words = str.split(" ");
+console.log(words[3]);
+// Expected output: "fox"
+
+// const chars = str.split("");
+// console.log(chars[8]);
+// Expected output: "k"
+
+// const strCopy = str.split();
+// console.log(strCopy);
+// Expected output: Array ["The quick brown fox jumps over the lazy dog."]
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  
 
