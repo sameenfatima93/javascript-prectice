6ebc73ef7dc20e4e1866fc1a9577a59bc99af201
SameenFatima <sameen.fatima993@gmail.com>
2025-08-16 09:07:13 +0500

    lecture11_start

---
+++ jsLecture_numberAndMathes/07_numAndMaths.js
@@ -0,0 +1,20 @@
+// const number = 356.3536
+// console.log(number.toPrecision(3));
+
+// tolocalestring
+
+const price = 1000000000
+console.log(price.toLocaleString(`en-UK`));
+
+
+// let name = "sameenfatima";
+
+// // Normal left → right
+// console.log(name.slice(0, 6));   // sameen
+
+// // Negative start
+// console.log(name.slice(-6));     // fatima
+
+// // Negative start aur positive end
+// console.log(name.slice(-11, 6)); // ameenf  (start = index 1, end = 6)
+
